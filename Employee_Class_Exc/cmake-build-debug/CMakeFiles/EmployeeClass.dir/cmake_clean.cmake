5012bf3c6b9854a918f9883ac853447cea32cd0e
Todd <todd.labo@ucdenver.edu>
2021-08-02 21:38:43 -0600

    Added the Employee Class exc

---
+++ Employee_Class_Exc/cmake-build-debug/CMakeFiles/EmployeeClass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EmployeeClass.dir/main.cpp.o"
+  "EmployeeClass.pdb"
+  "EmployeeClass.exe"
+  "libEmployeeClass.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EmployeeClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Employee_Class_Exc/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World Employees!" << std::endl;
+    return 0;
+}
